61500d2fa53a4e76bd25b50518f00de51c00340d
Wesley Smits <wesley@wsmits.nl>
2022-10-13 23:34:06 +0200

    Added login notifications

--- public/build/assets/Label.e30f5c4f.js
+++
@@ -1,1 +0,0 @@
-import{o as e,d as t,t as o,l}from"./app.4797f514.js";const n={class:"block font-medium text-sm text-gray-700"},r={key:0},c={key:1},m={__name:"Label",props:{value:String},setup(s){return(a,_)=>(e(),t("label",n,[s.value?(e(),t("span",r,o(s.value),1)):(e(),t("span",c,[l(a.$slots,"default")]))]))}};export{m as _};
