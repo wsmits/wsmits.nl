e05b8d124f403348ca9157e4ac7e1cad999cd7d7
Wesley Smits <wesley@wsmits.nl>
2022-11-01 23:06:31 +0100

    Added adddressbook page start

---
+++ public/build/assets/Label.f805b8fc.js
@@ -0,0 +1,1 @@
+import{o as e,d as t,t as o,l}from"./app.c3f2d526.js";const n={class:"block font-medium text-sm text-gray-700"},r={key:0},c={key:1},m={__name:"Label",props:{value:String},setup(s){return(a,_)=>(e(),t("label",n,[s.value?(e(),t("span",r,o(s.value),1)):(e(),t("span",c,[l(a.$slots,"default")]))]))}};export{m as _};
