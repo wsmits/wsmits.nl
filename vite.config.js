31a1b1480dea756ae7ddebf76ca64fca747e954b
Wesley Smits <wesley@wsmits.nl>
2025-11-07 08:58:38 +0100

    Add additional input files for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/js/app.js',
+                'resources/js/demo/europemap.js',
+                'resources/js/demo/worldmap.js',
+            ],
         }),
         vue({
             template: {
